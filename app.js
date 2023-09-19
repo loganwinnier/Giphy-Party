23761e7652f83022c6636b824dd3c2bf016f3317
Jessica Biondi <jbiondi.cs@gmail.com>
2023-09-19 19:46:43 -0400

    communicating with the api successfully

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
 /** performs get request and returns a  */
 async function getGif(event) {
   event.preventDefault();
+
   const gifTerm = $('#gif-term').val();
-  const response = await fetch();
+  const params = new URLSearchParams(
+    {q: gifTerm, api_key: `t0tue9BIc53NLhE4KaixTRC2pBVxLqRA`});
+
+  // url https://api.giphy.com/v1/gifs/search
+  const response = await fetch(`https://api.giphy.com/v1/gifs/search?${params}`);
+
+  const info = await response.json();
+  console.log(info);
 }
+
+// make some other functions
 
 $('#gif-submit').on('click', getGif);
